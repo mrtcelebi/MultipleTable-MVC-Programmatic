b12915d784564d787c874cacb44370aff623d06d
mrttcelebi <Murat Celebi>
2020-09-06 12:59:47 -0700

    Initial Commit

---
+++ MultipleTable/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipleTable
+//
+//  Created by Catalina on 6.09.2020.
+//  Copyright © 2020 Catalina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
